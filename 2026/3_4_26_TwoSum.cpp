500ee1c2882492c27efde7231b7ab5c876f4f955
O'Reilly <brendan2520@icloud.com>
2026-03-04 12:05:01 -0500

    added problem statement

--- 2026/3_4_26_TwoSum.cpp
+++ 2026/3_4_26_TwoSum.cpp
@@ -6,6 +6,24 @@
 
 using namespace std;
 
+
+/*
+Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Input Format
+
+A space separated array of numbers, followed by a target number on the next line.
+
+Output Format
+
+Two space separated integers representing the indices of the two numbers that add up to the target.
+*/
 int main(){
     //Input
     string temp; stringstream ss;
